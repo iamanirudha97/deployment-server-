eda69eb6b358293629ea5a4c63ef62381671450e
iamanirudha97 <sagarlm10@gmail.com>
2024-06-04 21:36:16 -0400

    Docker-and-Kubernetes

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-
-app.get('/docker', function (req, res) {
-  return res.json({ message: "Docker Containerization Practice"});
-});
-
-app.listen(process.env.PORT, () => console.log(`Server running on PORT : ${process.env.PORT}`));
